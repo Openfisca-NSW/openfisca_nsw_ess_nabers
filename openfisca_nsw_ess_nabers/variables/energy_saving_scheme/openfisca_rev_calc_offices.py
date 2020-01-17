1f25e5bba89e54575bbee76c3c39ddbd3258074e
Liam McCann <fromcassettes@gmail.com>
2020-01-17 12:23:41 +1100

    changes to reverse calculator

--- openfisca_nsw_ess_nabers/variables/energy_saving_scheme/openfisca_rev_calc_offices.py
+++ openfisca_nsw_ess_nabers/variables/energy_saving_scheme/openfisca_rev_calc_offices.py
@@ -86,21 +86,21 @@ class nabers_adjusted_hours(Variable):
         return input_hours + 10
 
 
-class elec_kWh (Variable):
+class elec_kWh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
     label = "The electricity consumption of the building, in kWh, as detailed on the NABERS report"
 
 
-class gas_kWh (Variable):
+class gas_kWh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
     label = "The gas consumption of the building, in kWh, as detailed on the NABERS report"
 
 
-class oil_kWh (Variable):
+class oil_kWh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
@@ -114,7 +114,7 @@ class coal_kWh (Variable):
     label = "The coal consumption of the building, in kWh, as detailed on the NABERS report"
 
 
-class total_energy_kwh (Variable):
+class total_energy_kwh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
@@ -124,7 +124,7 @@ class total_energy_kwh (Variable):
         return elec_kWh + gas_kWh + oil_kWh + coal_kWh
 
 
-class perc_elec_kwh (Variable):
+class perc_elec_kwh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
@@ -134,7 +134,7 @@ class perc_elec_kwh (Variable):
         return elec_kWh / total_energy_kwh * 100  # need to learn how to express this as a percentage
 
 
-class perc_gas_kwh (Variable):
+class perc_gas_kwh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
@@ -144,7 +144,7 @@ class perc_gas_kwh (Variable):
         return gas_kWh / total_energy_kwh * 100  # need to learn how to express this as a percentage
 
 
-class perc_diesel_kwh (Variable):
+class perc_diesel_kwh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
@@ -154,7 +154,7 @@ class perc_diesel_kwh (Variable):
         return diesel_kWh / total_energy_kwh * 100  # need to learn how to express this as a percentage
 
 
-class perc_oil_kwh (Variable):
+class perc_coal_kwh(Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
@@ -184,6 +184,17 @@ class f_base_building(Variable):
     def formula(buildings, period, parameters):
         adjusted_hours = buildings('maximum_nabers_adjusted_hours, period')
         return (1 / (0.38 + 0.0116 + adjusted_hours))
+
+
+class f_tenancy(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "factor for tenancy NABERS calculation"
+
+    def formula(buildings, period, parameters):
+        adjusted_hours = buildings('maximum_nabers_adjusted_hours')
+        return (1 / (0.38 + 0.0105 * adjusted_hours))
 
 
 class SGEgas (Variable):
@@ -324,7 +335,7 @@ class GEclimcorr (Variable):
     value_type = float
     entity = Building
     definition_period = ETERNITY
-    label = "climate corrected greenhouse emissions for the relevant NABERS building"  # check this
+    label = "climate corrected greenhouse emissions for the relevant NABERS whole building"  # check this
 
     def formula(buildings, period, parameters):
         Term1 = buildings('term_1', period)
@@ -333,3 +344,125 @@ class GEclimcorr (Variable):
         Term4 = buildings('term_4', period)
         Term5 = buildings('term_5', period)
         return Term1 + Term2 + Term3 + Term4 + Term5
+
+
+class GEClimcorr_tenancy (Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "weighted tenancy climate correct greenhouse emissions for the relevant NABERS building"
+
+    def formula(buildings, period, parameters):
+        return (4000 * 'SGE_tenancy' * (0.008 - Dequip))
+
+
+class coefficient_A (Variable):
+    value_type = float
+    entity = Building
+    definition_period = YEAR
+    label = "Rating equation coefficient_A used to calculate NABERS whole building ratings"
+
+    def formula(buildings, period, parameters):
+        state = buildings(building_state_location, period)
+        return select(
+            [state == "ACT", state == "NSW", state == "NT", state == "QLD", state == "SA", state == "TAS", state == "VIC", state == "WA"],
+            [6.7605, 6.7605, 6.422206, 7.1, 6.5247, 6.5351, 7.0114, 7.2857]
+            )
+
+
+class coefficient_B (Variable):
+    value_type = float
+    entity = Building
+    definition_period = YEAR
+    label = "Rating equation coefficient_A used to calculate NABERS whole building ratings"
+
+    def formula(buildings, period, parameters):
+        state = buildings(building_state_location, period)
+        return select(
+            [state == "ACT", state == "NSW", state == "NT", state == "QLD", state == "SA", state == "TAS", state == "VIC", state == "WA"],
+            [-0.0168067, -0.0168067, -0.03323, -0.02, -0.0166656, -0.0341818, -0.0444444, -0.05]
+            )
+
+
+class NGEmax (Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "Normalised greenhouse emissions for relevant NABERS whole building - only applicable under 5 stars"
+
+    def formula(buildings, period, parameters):
+        return ('benchmark_star_rating' - 0.499999 - 'coefficient_A') / 'coefficient_B'
+
+
+class GEwholemax (Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "Greenhouse emissions for relevant NABERS whole building - applicable above 5 stars"
+
+    def formula(buildings, period, parameters):
+        condition_GEwholemax_star_rating = buildings('benchmark_star_rating', period) >= 5
+        return where(condition_GEwholemax_star_rating, (NGEmax - f_base_building * GEclimcorr - f_tenancy * GEClimcorr_tenancy) * 2 / (f_base_building / f_tenancy), "not applicable")
+
+
+class NGE_5star_original_rating (Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "original normalised greenhouse emissions at 5 star for use for > 5 star ratings"
+
+    def formula(buildings, period, parameters):
+        return(5 - coefficient_A - 0.499999) / coefficient_B
+
+
+class GE_5star_original_rating(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "original required maximum greenhouse emissions at 5 star rating"
+
+    def formula(buildings, period, parameters):
+        return (NGEmax - f_base_building * GEclimcorr - f_tenancy * GEClimcorr_tenancy) * 2 / (f_base_building / f_tenancy)
+
+
+class GE_25_percent_reduction(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "25% reduction of required maximum greenhouse emissions for use in 5.5 star rating following original rating system"
+
+    def formula(buildings, period, parameters):
+        return GE_5star_original_rating * 0.75
+
+
+class GE_50_percent_reduction(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "50% reduction of required maximum greenhouse emissions for use in 6 star rating following original rating system"
+
+    def formula(buildings, period, parameters):
+        return GE_5star_original_rating * 0.5
+
+
+class maximum_electricity_consumption(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "output of the NABERS whole building reverse calculator - the maximum electricity consumption allowable for the relevant NABERS rated building"  # need to figure out how to round this to an integer
+
+    def formula(buildings, period, parameters):
+        return select(
+            [benchmark_star_rating <= 5, benchmark_star_rating == 5.5, benchmark_star_rating == 6],
+            [(GEwholemax * net_lettable_area) / (SGEelec + (perc_gas_kwh / perc_elec_kwh * SGEgas) + (perc_coal_kwh / perc_elec_kwh * SGEcoal) + (perc_diesel_kwh / perc_elec_kwh * SGEoil)), (GE_25_percent_reduction * net_lettable_area) / (SGE_elec + (perc_gas_kwh / perc_elec_kwh * SGEgas) + (perc_coal_kwh / perc_elec_kwh * SGEcoal) + (perc_diesel_kwh / perc_elec_kwh * SGEoil)), (GE_50_percent_reduction * net_lettable_area) / (SGE_elec + (perc_gas_kwh / perc_elec_kwh * SGEgas) + (perc_coal_kwh / perc_elec_kwh * SGEcoal) + (perc_diesel_kwh / perc_elec_kwh * SGEoil))]
+            )
+
+
+class maximum_gas_consumption(Variable):
+    value_type = float
+    entity = Building
+    definition_period = ETERNITY
+    label = "output of the NABERS whole building reverse calculator - the maximum electricity consumption allowable for the relevant NABERS rated building"
+
+    def formula(buildings, period, parameters):
+        return (perc_gas_kwh / perc_elec_kwh * maximum_electricity_consumption * 3.6)
