a1be956eb1f9a288d7d364005a21183e8f6a0945
Liam McCann <liam.mccann@environment.nsw.gov.au>
2020-04-15 09:49:09 +1000

    remove redundant code from building type, define required end variables for front end

--- openfisca_nsw_ess_nabers/variables/energy_savings_scheme/NABERS_ESS/building_type.py
+++ openfisca_nsw_ess_nabers/variables/energy_savings_scheme/NABERS_ESS/building_type.py
@@ -4,12 +4,6 @@ from openfisca_core.model_api import *
 from openfisca_nsw_base.entities import *
 
 
-# Import from openfisca-core the common Python objects used to code the legislation in OpenFisca
-from openfisca_core.model_api import *
-# Import the Entities specifically defined for this tax and benefit system
-from openfisca_nsw_base.entities import *
-
-
 class is_office(Variable):
     value_type = bool
     entity = Building
